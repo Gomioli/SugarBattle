c04eb83253c58913bea0138e761e8d5ae43c46dc
Gomioli <aliciabrunet86800@gmail.com>
2026-05-30 21:30:33 +0200

    Update PlayerController
    Maintenant quand on appuie sur T, ça fait apparaitre le GO spawnedObject

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,17 +8,21 @@ public class PlayerController : NetworkBehaviour
     [SerializeField] private Rigidbody playerRigidbody;
     [SerializeField] private float moveSpeed;
 
-
+    [SerializeField] private Transform spawnedObjectPrefab;
 
 
     void Update()
     {
         if (!IsOwner) return;
 
+        
         if (Input.GetKeyDown(KeyCode.T))
         {
-            TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } }); // Ca permet que seul le client d'ID 1 recoive un message, et pas le Host
+            Transform spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+            //TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } }); // Ca permet que seul le client d'ID 1 recoive un message, et pas le Host
         }
+        
 
         playerRigidbody.velocity = new Vector3(
             Input.GetAxis("Horizontal") * moveSpeed,
