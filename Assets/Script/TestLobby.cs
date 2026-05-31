adaedc904159e1877cf27d3767ef446071c99082
Gomioli <aliciabrunet86800@gmail.com>
2026-05-31 11:39:23 +0200

    Update TestLobby
    Ajout de la fonction Start(), pour que les services de Unity s'initialisent

--- Assets/Script/TestLobby.cs
+++ Assets/Script/TestLobby.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Core;
 using UnityEngine;
 
 public class TestLobby : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    private async void Start()
     {
-        
+        await UnityServices.InitializeAsync();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
