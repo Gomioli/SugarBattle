f5dbb7d687b2e925beed9dd5d1bdddb6e8865e9a
Gomioli <aliciabrunet86800@gmail.com>
2026-07-27 17:17:42 +0200

    Update script PlayerController
    Retrait des premiers test de multijoueur.
    + Ajout de deux varibales (jumpKey et isGrounded) pour coder la fonction Jump() qui vient d'être crée.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,50 +5,32 @@ using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
+
     [SerializeField] private Rigidbody playerRigidbody;
     [SerializeField] private float moveSpeed;
 
-    [SerializeField] private Transform spawnedObjectPrefab;
-    private Transform spawnedObjectTransform;
+    private KeyCode jumpKey = KeyCode.Space;
+    private bool isGrounded = true;
 
 
     void Update()
     {
-        if (!IsOwner) return;
 
-        
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
-            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
-            //TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } }); // Ca permet que seul le client d'ID 1 recoive un message, et pas le Host
-        }
-
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            spawnedObjectTransform.GetComponent<NetworkObject>().Despawn(true); 
-            //Destroy(spawnedObjectTransform.gameObject);
-        }
-        
 
         playerRigidbody.velocity = new Vector3(
             Input.GetAxis("Horizontal") * moveSpeed,
             0f,
             Input.GetAxis("Vertical") * moveSpeed);
+
+
+
     }
+    
+       
 
-
-    [ServerRpc]
-    private void TestServerRpc()
+    private void Jump()
     {
-        Debug.Log("TestServerRpc" + OwnerClientId);
-    }
 
-
-    [ClientRpc]
-    private void TestClientRpc(ClientRpcParams clientRpcParams)
-    {
-        Debug.Log("TestClientRpc");
     }
 
 }
