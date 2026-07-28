b256df58a8adc756afc834d2c10fb9c3fc003077
Gomioli <aliciabrunet86800@gmail.com>
2026-07-28 12:03:37 +0200

    Update SetupCameraPlayer -> ThirdPersonCam déplacé
    J'ai déplacé le script de ThirdPersonCam dans le script SetupCameraPlayer, car je dois prendre en compte le NetworkBehaviour.
    Ceci est un test, ce n'est pas sûr que ça fonctionne

--- Assets/Script/SetupCameraPlayer.cs
+++ Assets/Script/SetupCameraPlayer.cs
@@ -6,6 +6,26 @@ using Cinemachine;
 
 public class SetupCameraPlayer : NetworkBehaviour // donne accès aux fonctionnalités du réseau Netcode.
 {
+
+    [SerializeField] Transform orientation;
+    [SerializeField] Transform player;
+    [SerializeField] Transform playerObj;
+    [SerializeField] Rigidbody rb;
+
+    public float rotationSpeed;
+
+    [SerializeField] Transform combatLookAt;
+
+    public CameraStyle currentStyle;
+
+    public enum CameraStyle
+    {
+        Basic,
+        Combat
+    }
+
+
+
     public override void OnNetworkSpawn() // est appelée automatiquement une fois que l'objet réseau a fini de spawn. Similaire à un Start().
     {
         if (IsOwner)
@@ -14,5 +34,17 @@ public class SetupCameraPlayer : NetworkBehaviour // donne accès aux fonctionnal
             freeLook.Follow = transform;
             freeLook.LookAt = transform;
         }
+    }
+
+
+
+    private void Update()
+    {
+        // tourne le GO orientation
+        Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+        orientation.forward = dirToCombatLookAt.normalized;
+
+        // tourne le GO playerObj
+        playerObj.forward = dirToCombatLookAt.normalized;
     }
 }
