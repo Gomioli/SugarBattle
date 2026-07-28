cd01e3b174b491355b8f7f487009c60324abbfe0
Gomioli <aliciabrunet86800@gmail.com>
2026-07-28 12:00:30 +0200

    Update ThirdPersonCam -> changement de nom
    Le nom de la class Third a été changé en ThirdPersonCam

--- Assets/Script/ThirdPersonCam.cs
+++ Assets/Script/ThirdPersonCam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Third : MonoBehaviour
+public class ThirdPersonCam : MonoBehaviour
 {
 
     [SerializeField] Transform orientation;
